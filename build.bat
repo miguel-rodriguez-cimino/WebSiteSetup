64d7156a92d884216c25084080403804c479f638
Miguel Rodriguez Cimino <mig_rodriguez@yahoo.com>
2019-11-01 09:46:55 -0300

    updated to msbuild 16

--- build.bat
+++ build.bat
@@ -1,3 +1,4 @@
 @ECHO OFF
-"c:\windows\microsoft.net\framework\v4.0.30319\msbuild.exe" build.proj /p:VisualStudioVersion=12.0
+rem "c:\windows\microsoft.net\framework\v4.0.30319\msbuild.exe" build.proj /p:VisualStudioVersion=12.0
+"C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\msbuild.exe" build.proj /p:VisualStudioVersion=12.0
 pause
